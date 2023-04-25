75cd1322b7ec70101c95590c3bd100c75c0a30f7
Robertoes <robertoes2697@gmail.com>
2023-04-25 03:47:59 -0500

    Initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,64 @@
+const path = require('path');
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:react/recommended', 'plugin:import/recommended', 'plugin:jsx-a11y/recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      alias: {
+        map: [['@/assets', './src/assets'], ['@/components', './src/components'], ['@/data', './src/data']],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+      }
+    }
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
+  },
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json'),
+    sourceType: 'module'
+  },
+  rules: {
+    // TypeScript rules
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    // General rules
+
+    'no-var': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'no-multi-spaces': 'error',
+    eqeqeq: ['error', 'always'],
+    'no-mixed-operators': 'error',
+    'prefer-arrow-callback': 'error',
+    'prettier/prettier': ['error', require('./prettierrc.cjs')],
+    // React rules
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    // Import rules
+    'import/order': ['error', {
+      groups: [['builtin', 'external'], 'internal', ['parent', 'sibling', 'index']],
+      'newlines-between': 'always'
+    }]
+  }
+};
